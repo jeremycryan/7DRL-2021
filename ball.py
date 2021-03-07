7c538cf7fe004236fd91bee7ce7c9149c4db4afa
jeremycryan <jeremychristopherryan@gmail.com>
2021-03-07 11:11:58 -0600

    Add Cue object

--- ball.py
+++ ball.py
@@ -6,6 +6,7 @@ from primitives import PhysicsObject, Pose
 class Ball(PhysicsObject):
     def __init__(self, game, x=0, y=0, radius=20):
         self.radius = radius
+        self.weight = 1
         self.color = (255, 0, 0)  # This won't matter once we change drawing code
         super().__init__(game, (x, y), 0)
 
@@ -31,11 +32,13 @@ class Ball(PhysicsObject):
 
     def knock(self, cue, angle, power):
         # TODO implement this
-        # TODO create cue object
         # Angle should be the angle, in degrees counterclockwise from --> right -->
         # Power should be a float from 0-100 indicating how hard you're hitting. This might not map 1:1 to velocity
         # depending on the Cue object.
-        pass
+        speed = cue.power_to_speed(power=power, ball=self)
+        velocity_vector = Pose((speed, 0), 0)
+        velocity_vector.rotate_position(angle)
+        self.velocity += velocity_vector
 
     def draw(self, screen, offset=(0, 0)):
         x, y = self.pose.get_position()
--- player.py
+++ player.py
@@ -1,13 +1,17 @@
+import math
+
 import pygame
 
 from ball import Ball
 from primitives import Pose
+from cue import Cue, BasicCue
 
 
 class Player(Ball):
     def __init__(self, x=0, y=0):
         super().__init__(x, y, 20)
         self.color = (255, 255, 0)
+        self.active_cue = BasicCue()
 
     def update(self, dt, events):
         for event in events:
@@ -21,5 +25,10 @@ class Player(Ball):
 
     def cue_hit(self, hit_vector):
         # TODO use self.knock, and account for cue type
-        self.velocity = hit_vector.copy()
-        print(self.velocity)
+        # self.velocity = hit_vector.copy()
+        angle = math.atan2(-hit_vector.y, hit_vector.x) * 180/math.pi
+        power = hit_vector.magnitude()/5
+        if power > 100:
+            power = 100
+        self.velocity *= 0
+        self.knock(self.active_cue, angle, power)
